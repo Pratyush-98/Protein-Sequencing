441871b6fa74749ec9bacb6b4ef9120ec7f28497
Pratyush-98 <pratyushsharma899@msitprogram.net>
2021-11-12 16:21:34 +0530

    completed common proteins functions

--- hw6_protein.py
+++ hw6_protein.py
@@ -4,6 +4,7 @@ Name:
 Roll Number:
 """
 
+from typing import List
 import hw6_protein_tests as test
 
 project = "Protein" # don't edit this
@@ -120,7 +121,13 @@ Parameters: 2D list of strs ; 2D list of strs
 Returns: 2D list of strs
 '''
 def commonProteins(proteinList1, proteinList2):
-    return
+    List_=[]
+    for arow in proteinList1:
+        for brow in proteinList2:
+            if arow==brow and arow not in List_:
+                List_.append(arow)
+
+    return List_
 
 
 '''
@@ -229,18 +236,18 @@ def runFullProgram():
 
 # This code runs the test cases to check your work
 if __name__ == "__main__":
-    print("\n" + "#"*15 + " WEEK 1 TESTS " +  "#" * 16 + "\n")
-    test.week1Tests()
-    print("\n" + "#"*15 + " WEEK 1 OUTPUT " + "#" * 15 + "\n")
-    runWeek1()
+    # print("\n" + "#"*15 + " WEEK 1 TESTS " +  "#" * 16 + "\n")
+    # test.week1Tests()
+    # print("\n" + "#"*15 + " WEEK 1 OUTPUT " + "#" * 15 + "\n")
+    # runWeek1()
 
     ## Uncomment these for Week 2 ##
-    """
+
     print("\n" + "#"*15 + " WEEK 2 TESTS " +  "#" * 16 + "\n")
     test.week2Tests()
     print("\n" + "#"*15 + " WEEK 2 OUTPUT " + "#" * 15 + "\n")
     runWeek2()
-    """
+   
 
     ## Uncomment these for Week 3 ##
     """
